2d2698583d04e2168e1b59fbc1c1c3f2116e5ea5
leothorp <leothorp@users.noreply.github.com>
2022-11-25 23:58:50 -0700

    incr

--- services/client/src/App.tsx
+++ services/client/src/App.tsx
@@ -1,8 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { httpBatchLink } from "@trpc/client";
 import { useState } from "react";
+import { trpc } from "utils/trpc";
+// import { trpc } from "../../services/client/src/utils/trpc";
 import { Greeting } from "./Greeting";
-import { trpc } from "./utils/trpc";
+// import { trpc } from "./utils/trpc";
 
 const App = () => {
   const [queryClient] = useState(() => new QueryClient());
