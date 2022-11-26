b8a621712c348fe75cd58c9919e3b82dc41c4544
leothorp <leothorp@users.noreply.github.com>
2022-11-26 14:59:03 -0700

    rm lint-staged

--- packages/utilities/src/utils.ts
+++ packages/utilities/src/utils.ts
@@ -1,4 +1,4 @@
-// eslint-disable @typescript-eslint/no-explicit-any
+/* eslint-disable @typescript-eslint/no-explicit-any */
 export const toObj = <T extends object>(
   arr: T[],
   getKey: (arrEl: T) => string,
