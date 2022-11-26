1553d9797d230b7c31593216b0ccff92724dd8d3
leothorp <leothorp@users.noreply.github.com>
2022-11-25 23:17:35 -0700

    add

---
+++ services/server/src/server/server.ts
@@ -0,0 +1,42 @@
+import ws from "@fastify/websocket";
+import { fastifyTRPCPlugin } from "@trpc/server/adapters/fastify";
+import fastify from "fastify";
+import { appRouter } from "./router";
+import { createContext } from "./router/context";
+
+export interface ServerOptions {
+  dev?: boolean;
+  port?: number;
+  prefix?: string;
+}
+
+export function createServer(opts: ServerOptions) {
+  const dev = opts.dev ?? true;
+  const port = opts.port ?? 3000;
+  const prefix = opts.prefix ?? "/trpc";
+  const server = fastify({ logger: dev });
+
+  server.register(ws);
+  server.register(fastifyTRPCPlugin, {
+    prefix,
+    useWSS: true,
+    trpcOptions: { router: appRouter, createContext },
+  });
+
+  server.get("/", async () => {
+    return { hello: "wait-on 💨" };
+  });
+
+  const stop = () => server.close();
+  const start = async () => {
+    try {
+      await server.listen(port);
+      console.log("listening on port", port);
+    } catch (err) {
+      server.log.error(err);
+      process.exit(1);
+    }
+  };
+
+  return { server, start, stop };
+}
