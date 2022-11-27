13cb5c4c749bba3129ac31bb4320fda107f24bc1
leothorp <leothorp@users.noreply.github.com>
2022-11-26 18:55:21 -0700

    repo script

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -2,5 +2,5 @@
 set -uex
 pnpm -v
 npx pnpm@latest -v
-npx pnpm@latest i --frozen-lockfile --prefer-offline --filter $1
-npx pnpm@latest exec dotenv -- npx pnpm@latest run build --filter $1
+pnpm i --frozen-lockfile --prefer-offline
+pnpm exec dotenv -- pnpm run build --filter $1
