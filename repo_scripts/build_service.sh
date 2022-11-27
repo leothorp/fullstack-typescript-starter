6b5848e5a24cf79f51c6bcd7cce4a295d8ecd561
leothorp <leothorp@users.noreply.github.com>
2022-11-26 20:03:52 -0700

    inst ts

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -3,6 +3,10 @@ set -e
 echo "pnpm version: $(pnpm -v)"
 
 
+# basic CI checks (skip for now)
+# pnpx eslint@latest --no-error-on-unmatched-pattern --ignore-path .gitignore --ext .jsx,.js,.ts,.tsx services/$1
+
+
 pnpm i --frozen-lockfile --ignore-scripts
 
 node_modules/.bin/dotenv -- pnpm run --filter $1 build 
