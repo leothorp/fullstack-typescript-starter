3a398de13b07ce8c586c9d7fc4bb445fc947b4bf
leothorp <leothorp@users.noreply.github.com>
2022-11-27 16:45:26 -0700

    husky

--- services/server/src/index.ts
+++ services/server/src/index.ts
@@ -2,12 +2,10 @@
 // import { serverConfig } from "@server/config";
 import "reflect-metadata";
 import { serverConfig } from "@server/config";
-import { initializeDB } from "@server/_old_mikorm_database/db";
 import { createServer } from "@server/server";
 
 const init = async () => {
-  const orm = await initializeDB();
-  const server = await createServer(serverConfig, orm);
+  const server = await createServer(serverConfig);
 
   server.start();
 };
