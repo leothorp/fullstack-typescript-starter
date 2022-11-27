0eef7791722cec2a7dcbe35af4148db50305c1b8
leothorp <leothorp@users.noreply.github.com>
2022-11-27 00:05:31 -0700

    rm dev

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -9,8 +9,7 @@ cd "$(dirname $0)/.."
 # pnpx eslint@latest --no-error-on-unmatched-pattern --ignore-path .gitignore --ext .jsx,.js,.ts,.tsx services/$1
 # cd services/$1 && tsc --noEmit
 
-# certain scripts in devDependencies needed for the build process
-NODE_ENV=development pnpm i --frozen-lockfile --ignore-scripts
+pnpm i --frozen-lockfile --ignore-scripts
 
 
 pnpm run --filter $1 build 
