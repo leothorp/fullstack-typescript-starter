207aba49f8fc18b5e06b8a1e1ab55829b0126629
leothorp <leothorp@users.noreply.github.com>
2022-11-26 21:12:18 -0700

    add

--- packages/utilities/src/shared-constants.ts
+++ packages/utilities/src/shared-constants.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
 export const NODE_ENV = process.env.NODE_ENV!;
 export const isDevelopment = NODE_ENV === "development";
 export const isProduction = NODE_ENV === "production";
