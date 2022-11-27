2ac8561db20009d9a5e23d7cb20954a4573f9f58
leothorp <leothorp@users.noreply.github.com>
2022-11-26 19:34:58 -0700

    build

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -uex
 echo "pnpm version: $(pnpm -v)"
+pwd
 pnpm i -D --frozen-lockfile --prefer-offline --ignore-scripts
 node_modules/.bin/dotenv -- pnpm run --filter $1 build 
