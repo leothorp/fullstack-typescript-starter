501e7bfe6273125cf52c883100a9d9b81eef9bf8
leothorp <leothorp@users.noreply.github.com>
2022-11-27 23:36:49 -0700

    fix

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -9,7 +9,7 @@ pnpm i --frozen-lockfile
 
 
 # basic CI checks
-pnpx eslint --no-error-on-unmatched-pattern --ignore-path .gitignore --ext .jsx,.js,.ts,.tsx services/$1
+node_modules/.bin/eslint --no-error-on-unmatched-pattern --ignore-path .gitignore --ext .jsx,.js,.ts,.tsx services/$1
 cd services/$1 && ../../node_modules/.bin/tsc --noEmit 
 
 cd "$(dirname $0)/.."
