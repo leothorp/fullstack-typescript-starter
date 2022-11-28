9839a5c11738dd90290f7c8c14949b7bc046dfc9
leothorp <leothorp@users.noreply.github.com>
2022-11-28 00:08:37 -0700

    pclient

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -10,7 +10,7 @@ pnpm i --frozen-lockfile
 
 # basic CI checks
 node_modules/.bin/eslint --no-error-on-unmatched-pattern --ignore-path .gitignore --ext .jsx,.js,.ts,.tsx services/$1
-cd services/$1 && ../../node_modules/.bin/tsc --noEmit 
+node_modules/.bin/tsc --noEmit --project services/$1/tsconfig.json
 
 cd "$(dirname $0)/.."
 pnpm run --filter $1 build 
