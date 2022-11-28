e75e29abc49bc7a5d12bf10a5f0a9afeea1c7075
leothorp <leothorp@users.noreply.github.com>
2022-11-27 23:08:06 -0700

    done

--- repo_scripts/build_service.sh
+++ repo_scripts/build_service.sh
@@ -5,7 +5,7 @@ echo "pnpm version: $(pnpm -v)"
 cd "$(dirname $0)/.."
 
 
-# basic CI (skip for now)
+# basic CI checks
 pnpx eslint@latest --no-error-on-unmatched-pattern --ignore-path .gitignore --ext .jsx,.js,.ts,.tsx services/$1
 cd services/$1 && tsc --noEmit
 
