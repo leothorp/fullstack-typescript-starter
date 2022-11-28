138123b169ef00fcf6aa23a4945cc2bd2173c46f
leothorp <leothorp@users.noreply.github.com>
2022-11-27 23:16:17 -0700

    latest'

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,9 +3,4 @@ module.exports = {
   // Load shared config from ./packages/eslint-config-shared.
   // individual workspaces can have their own .eslinrc files that also extend this.
   extends: ["shared"],
-  rules: {
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/no-non-null-assertion": "off",
-  },
 };
--- packages/eslint-config-shared/index.js
+++ packages/eslint-config-shared/index.js
@@ -1,4 +1,8 @@
 module.exports = {
   extends: ["plugin:@typescript-eslint/recommended", "prettier"],
-  rules: {},
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
 };
