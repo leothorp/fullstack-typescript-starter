15f687a9ba3b34d21552feed04e035e1ef4fa418
leothorp <leothorp@users.noreply.github.com>
2022-11-28 22:25:15 -0700

    ta

--- services/client/src/forms/BaseForm.tsx
+++ services/client/src/forms/BaseForm.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, FormEventHandler } from "react";
+import React, { ReactNode, FormEventHandler, Ref } from "react";
 // import { FC, PropsWithChildren } from "@types/react";
 const BaseForm = (props: {
   onSubmit: FormEventHandler<HTMLFormElement>;
@@ -8,21 +8,25 @@ const BaseForm = (props: {
 };
 
 export default BaseForm;
-export const BaseInput = React.forwardRef((props, ref) => {
-  return (
-    <input
-      {...props}
-      ref={ref}
-      className="min-w-[200px] border-2 border-solid border-lightgray mt-2"
-    />
-  );
-});
-export const BaseTextarea = React.forwardRef((props, ref) => {
-  return (
-    <textarea
-      {...props}
-      ref={ref}
-      className="min-w-[200px] border-2 border-solid border-lightgray mt-2"
-    />
-  );
-});
+export const BaseInput = React.forwardRef(
+  (props, ref: Ref<HTMLInputElement>) => {
+    return (
+      <input
+        {...props}
+        ref={ref}
+        className="min-w-[200px] border-2 border-solid border-lightgray mt-2"
+      />
+    );
+  }
+);
+export const BaseTextarea = React.forwardRef(
+  (props, ref: Ref<HTMLTextAreaElement>) => {
+    return (
+      <textarea
+        {...props}
+        ref={ref}
+        className="min-w-[200px] border-2 border-solid border-lightgray mt-2"
+      />
+    );
+  }
+);
