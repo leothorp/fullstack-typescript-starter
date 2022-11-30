87b832565d58145ed4daaf2d083ba91cf7436ab6
leothorp <leothorp@users.noreply.github.com>
2022-11-30 00:25:57 -0700

    init

--- repo_scripts/prisma_init.sh
+++ repo_scripts/prisma_init.sh
@@ -4,17 +4,16 @@ set -e
 # This script generates a Prisma schema file/migrations from an existing db (leaving its data intact).
 
 
-cd services/server
 
-pnpm run prisma-cli init
-pnpm run prisma-cli db pull
+# pnpm run prisma-cli init
+# pnpm run prisma-cli db pull
 
 
 # generate initial migration file, mark it as applied
 CURR_TIMESTAMP=$(date -u "+%Y%m%d%H%M%S")
 mkdir -p ./prisma/migrations/${CURR_TIMESTAMP}_init
-pnpm run prisma-cli migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > ./prisma/migrations/${CURR_TIMESTAMP}_init/migration.sql
-pnpm run prisma-cli migrate resolve --applied ${CURR_TIMESTAMP}_init
+pnpm run --silent prisma-cli migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma --script > ./prisma/migrations/${CURR_TIMESTAMP}_init/migration.sql
+pnpm run --silent prisma-cli migrate resolve --applied ${CURR_TIMESTAMP}_init
 
 
 
@@ -24,7 +23,7 @@ pnpm run prisma-cli migrate resolve --applied ${CURR_TIMESTAMP}_init
 # incorrect instructions below from Prisma docs at https://www.prisma.io/docs/getting-started/setup-prisma/add-to-existing-project/relational-databases/introspection-typescript-postgres
 # these result in a non-timestamped "init" migration that gets skipped on subsequent re-runs from a reset db.
 # *****
-mkdir -p ./prisma/migrations/init
+# mkdir -p ./prisma/migrations/init
 # pnpx -- prisma migrate diff --from-empty --to-schema-datamodel prisma/schema.prisma
 
 # mark init migration as applied:
