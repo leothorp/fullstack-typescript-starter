bdb37aac9011acacaa2ba6b578b6fcc6c130be20
Stuart Leach <jstuartleach@gmail.com>
2023-03-15 09:27:37 -0400

    Add comments

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+# Copy conents of .env.*.example files to new .env.*.local files
 cat .env.server.example > .env.server.local
 cat .env.shared.example > .env.shared.local
 
@@ -9,6 +10,7 @@ jwt_secret=$(openssl rand -hex 64)
 # Replace the JWT secret in the .env.server.local file
 sed -i '' -e "s/insert-secret-here/$jwt_secret/g" .env.server.local
 
+# Install dependencies
 pnpm i
 
 # Optional: delete this file when the actions are completed to avoid overwriting the .env files and jwt_secret.
