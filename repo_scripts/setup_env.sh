ec2dc6f5fe946f34459ff41d173899a23db076c1
Stuart Leach <jstuartleach@gmail.com>
2023-03-16 22:36:13 -0400

    Update repo_scripts/setup_env.sh
    nice
    
    Co-authored-by: Leo Thorp <leothorp@users.noreply.github.com>

--- repo_scripts/setup_env.sh
+++ repo_scripts/setup_env.sh
@@ -2,7 +2,10 @@
 
 # change directory to the project's root, one-level up from the current directory
 cd $(dirname "$0")/..
-
+if [[ -f .env.server.local ]] || [[ -f .env.shared.local ]]; then
+  echo "Error: local env files already exist."
+  exit 1
+fi
 # Copy contents of .env.*.example files to new .env.*.local files
 
 cat .env.server.example > .env.server.local
