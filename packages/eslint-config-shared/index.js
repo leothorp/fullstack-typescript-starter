59e2faa5dcc48dd8ef118c9e04814521743b5397
leothorp <leothorp@users.noreply.github.com>
2023-03-16 22:08:18 -0500

    check

--- packages/eslint-config-shared/index.js
+++ packages/eslint-config-shared/index.js
@@ -4,5 +4,6 @@ module.exports = {
     "@typescript-eslint/no-unused-vars": "error",
     "@typescript-eslint/ban-ts-comment": "off",
     "@typescript-eslint/no-non-null-assertion": "off",
+    "no-debugger": "error",
   },
 };
